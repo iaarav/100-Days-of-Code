eefee92efcbfd73d0a390cb210c679b454994c3b
iaarav <aarav2008bansal@gmail.com>
2024-08-01 20:03:06 +0530

    Day 60

--- Advanced/Day 58, 59 and 60/Day 58, 59 and 60.py
+++ Advanced/Day 58, 59 and 60/Day 58, 59 and 60.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-import pprint
 
 blogs = requests.get('https://api.npoint.io/c3a80236a09977004744').json()
-pprint.pprint(blogs)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", blogs = blogs)
+    return render_template("index.html", blogs=blogs)
 
 
 @app.route("/about")
@@ -18,9 +16,18 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+
+        return render_template("contact.html", msg_sent=True)
+
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/index/<int:index>")
@@ -33,6 +40,6 @@ def blogPost(index):
 
     return render_template("post.html", post=requestedBlog)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
